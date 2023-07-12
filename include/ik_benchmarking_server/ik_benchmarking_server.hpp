7da143f5e70c19f4dd7ab794da7340d8e381d632
Robotawi <mohamed.s.raessa@gmail.com>
2023-07-12 16:35:55 +0900

    Split IKBenchmarkingServer into .hpp and .cpp files

---
+++ include/ik_benchmarking_server/ik_benchmarking_server.hpp
@@ -0,0 +1,32 @@
+#ifndef IK_BENCHMARKING_SERVER_HPP
+#define IK_BENCHMARKING_SERVER_HPP
+
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+
+#include "ik_benchmarking/action/ik_benchmark.hpp"
+#include "ik_benchmarking/ik_benchmarking.hpp"
+
+using IKBenchmark = ik_benchmarking::action::IKBenchmark;
+using GoalHandleIKBenchmark = rclcpp_action::ServerGoalHandle<IKBenchmark>;
+
+class IKBenchmarkingServer : public rclcpp::Node
+{
+public: 
+  IKBenchmarkingServer(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
+
+private:
+  rclcpp_action::Server<IKBenchmark>::SharedPtr action_server_; 
+
+  rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid, 
+  std::shared_ptr<const IKBenchmark::Goal> goal);
+
+  rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleIKBenchmark> goal_handle);
+
+  void handle_accepted(const std::shared_ptr<GoalHandleIKBenchmark> goal_handle);
+
+  void execute(const std::shared_ptr<GoalHandleIKBenchmark> goal_handle);
+};
+
+#endif //IK_BENCHMARKING_SERVER_HPP
